7a49e8bae3973fc15f91bb319db34ffae29198f1
rlaeksl <abcdy11@naver.com>
2022-12-28 13:35:14 +0900

    [추가] globalthis, datatypes

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,50 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+/* eslint no-unused-vars: 0 */
+let admin;
+let name;
+
+name = 'John';
+
+admin = name;
+
+console.log(admin);
+
+
+let ourPlanetName = 'Earth';
+let loginUserName = 'KimDaYeon';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기 - 변수
+let calcProductQuantity;
+
+// - 구매 제품 가격의 총 합 - 변수
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수 - 상수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부 - 상수
+let isPayment = false;
+let hasClassName = false;
+
+// - 구매 결제 내역 - 상수
+let paymentHistory;
+
+// - 브랜드 접두사 - 상수
+const BRAND_PREFIX = 'EUID';
+
+// - 오늘의 운세 - 변수
+let fortuneOfToday;
+
+// - 상품 정보 - 상수
+const productInfo = '';
+
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
