4ea41e0f7615a2342719dba3a981f864fd14aa28
rlaeksl <abcdy11@naver.com>
2023-01-13 11:01:19 +0900

    최종

--- client/main.js
+++ client/main.js
@@ -46,4 +46,18 @@ function handler(e){
   insertLast(result,total);
 }
 
+
+function inputHandler(){
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  let total = sum(firstValue,secondValue);
+
+  clearContents(result);
+
+  insertLast(result,total);
+}
+
 done.addEventListener('click',handler)
+
+firstInput.addEventListener('change',inputHandler)
+secondInput.addEventListener('change',inputHandler)
